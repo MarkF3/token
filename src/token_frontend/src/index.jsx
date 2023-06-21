6a943f20bd5101e51722ae20d60619002bd49431
Mark Farrell <Markpeterfarrell@gmail.com>
2023-06-21 19:19:09 +0100

    Front-end authentication

--- src/token_frontend/src/index.jsx
+++ src/token_frontend/src/index.jsx
@@ -1,14 +1,40 @@
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from "./components/App";
+import {AuthClient} from "@dfinity/auth-client";
+
+
 
 const init = async () => { 
-  const root = ReactDOM.createRoot( document.getElementById("root"));
-  root.render(
+ 
+
+  const authClient = await AuthClient.create();
+
+  if(await authClient.isAuthenticated()){
+
+    handleAuthenticated(authClient)
+
     
-      <App />
-    
-  );
+  } else{
+
+    await authClient.login({
+
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => { handleAuthenticated(authClient) }
+  
+    })
+  }
+
+  async function handleAuthenticated(authClient) {
+
+    const root = ReactDOM.createRoot( document.getElementById("root"));
+    root.render(<App />);
+
+
+  } 
+
+ 
+
 }
 
 init();
